73dd04167a1536622ceda208f942b9eb44053f81
Margie Lehmann <mlehman1@binghamton.edu>
2017-09-14 18:08:42 +0000

    win and lose checks

--- app.rb
+++ app.rb
@@ -40,6 +40,7 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    @game.guess(letter)
     redirect '/show'
   end
   
@@ -50,6 +51,11 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
+    if @game.check_win_or_lose == :win
+      redirect '/win'
+    elsif @game.check_win_or_lose == :lose
+      redirect '/lose'
+    end
     erb :show # You may change/remove this line
   end
   
